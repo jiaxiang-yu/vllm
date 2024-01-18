e938ae571ca6942a757a3357088ac9d20f814565
Jiaxiang Yu <yujiaxiang2013@gmail.com>
2024-01-18 17:54:05 +0800

    senario 2 run

--- benchmarks/cllam/run.sh
+++ benchmarks/cllam/run.sh
@@ -1,19 +1,19 @@
-start=$(date +%s.%N)
-python benchmarks/cllam/sweep.py --model ~/data/models/vicuna-7b-v1.3/ \
-        --tokenizer ~/data/models/vicuna-7b-v1.3/ \
-        --dataset ~/data/ShareGPT_V3_unfiltered_cleaned_split.json \
-        --vllm_dir ~/vllm \
-        --prompt_len 64 \
-        --gen_len 256 \
-        --repeat_num 1 \
-        --request_rate_params 1,32,1 \
-        --num_requests 1000
-end=$(date +%s.%N)
-runtime=$(python -c "print(${end} - ${start})")
+# start=$(date +%s.%N)
+# python benchmarks/cllam/sweep.py --model ~/data/models/vicuna-7b-v1.3/ \
+#         --tokenizer ~/data/models/vicuna-7b-v1.3/ \
+#         --dataset ~/data/ShareGPT_V3_unfiltered_cleaned_split.json \
+#         --vllm_dir ~/vllm \
+#         --prompt_len 64 \
+#         --gen_len 256 \
+#         --repeat_num 1 \
+#         --request_rate_params 1,32,1 \
+#         --num_requests 1000
+# end=$(date +%s.%N)
+# runtime=$(python -c "print(${end} - ${start})")
 
-mv ~/vllm/cllam/trace ~/vllm/cllam/64-256
-mv ~/vllm/data/* ~/vllm/cllam/64-256
-echo "Runtime: $runtime sec" >> ~/vllm/cllam/64-256/done
+# mv ~/vllm/cllam/trace ~/vllm/cllam/64-256
+# mv ~/vllm/data/* ~/vllm/cllam/64-256
+# echo "Runtime: $runtime sec" >> ~/vllm/cllam/64-256/done
 
 
 # start=$(date +%s.%N)
@@ -49,5 +49,22 @@ echo "Runtime: $runtime sec" >> ~/vllm/cllam/64-256/done
 # mv ~/vllm/cllam/trace ~/vllm/cllam/64-128
 # mv ~/vllm/data/* ~/vllm/cllam/64-128
 # echo "Runtime: $runtime sec" >> ~/vllm/cllam/64-128/done
+
+start=$(date +%s.%N)
+python benchmarks/cllam/sweep.py --model ~/data/models/vicuna-7b-v1.3/ \
+        --tokenizer ~/data/models/vicuna-7b-v1.3/ \
+        --dataset ~/data/ShareGPT_V3_unfiltered_cleaned_split.json \
+        --vllm_dir ~/vllm \
+        --prompt_len 800 \
+        --gen_len 64 \
+        --repeat_num 1 \
+        --request_rate_params 1,32,1 \
+        --num_requests 1000
+end=$(date +%s.%N)
+runtime=$(python -c "print(${end} - ${start})")
+
+mv ~/vllm/cllam/trace ~/vllm/cllam/800-64
+mv ~/vllm/data/* ~/vllm/cllam/800-64
+echo "Runtime: $runtime sec" >> ~/vllm/cllam/800-64/done
 
 touch ~/vllm/cllam/all_done
