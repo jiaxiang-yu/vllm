739d6ac74b309eb1c6fa6e5956f654e86b080461
Jiaxiang Yu <yujiaxiang2013@gmail.com>
2024-02-07 12:24:58 +0800

    adjust scripts for llama2

--- benchmarks/cllam/tp_70b_a10g.sh
+++ benchmarks/cllam/tp_70b_a10g.sh
@@ -1,13 +1,14 @@
 start=$(date +%s.%N)
-python benchmarks/cllam/sweep.py --model ~/data/models/llama-2-7b-hf/ \
-        --tokenizer ~/data/models/llama-2-7b-hf/ \
+python benchmarks/cllam/sweep.py --model ~/data/models/llama-2-70b-hf/ \
+        --tokenizer ~/data/models/llama-2-70b-hf/ \
         --dataset ~/data/ShareGPT_V3_unfiltered_cleaned_split.json \
         --vllm_dir ~/vllm \
         --prompt_len 64 \
         --gen_len 256 \
         --repeat_num 1 \
-        --request_rate_params 1,32,1 \
-        --num_requests 1000
+        --request_rate_params 1,4,1 \
+        --num_requests 100 \
+        --tensor_parallel_size 8
 end=$(date +%s.%N)
 runtime=$(python -c "print(${end} - ${start})")
 
@@ -16,15 +17,16 @@ mv ~/vllm/data/* ~/vllm/cllam/64-256
 echo "Runtime: $runtime sec" >> ~/vllm/cllam/64-256/done
 
 start=$(date +%s.%N)
-python benchmarks/cllam/sweep.py --model ~/data/models/llama-2-7b-hf/ \
-        --tokenizer ~/data/models/llama-2-7b-hf/ \
+python benchmarks/cllam/sweep.py --model ~/data/models/llama-2-70b-hf/ \
+        --tokenizer ~/data/models/llama-2-70b-hf/ \
         --dataset ~/data/ShareGPT_V3_unfiltered_cleaned_split.json \
         --vllm_dir ~/vllm \
         --prompt_len 800 \
         --gen_len 64 \
         --repeat_num 1 \
-        --request_rate_params 1,32,1 \
-        --num_requests 1000
+        --request_rate_params 1,4,1 \
+        --num_requests 100 \
+        --tensor_parallel_size 8
 end=$(date +%s.%N)
 runtime=$(python -c "print(${end} - ${start})")
 
